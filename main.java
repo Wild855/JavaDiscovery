6b788526b1a81ce71b4cd25a99a70281e5da1178
Wild855 <126831152+Wild855@users.noreply.github.com>
2025-04-17 01:38:42 -0400

    Add new classes and enhance main.java with documentation and interestCalc method

--- main.java
+++ main.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
+/**
+ * The main class serves as the entry point for the Java application.
+ * It creates an instance of the BrianGriffin class and invokes its methods.
+ *
+ * Methods:
+ * - main(String[] args): The main method that executes the program.
+ *
+ * Usage:
+ * - Creates an object of the BrianGriffin class.
+ * - Calls the `speak` and `fart` methods on the BrianGriffin object.
+ * - Outputs a blank line to the console.
+ */
 public class main {
     public static void main(String args[]) {
 
@@ -9,7 +21,15 @@ public class main {
         Familyguy.fart();
 
         System.out.println();
+
+        interestCalc();
     }
+
+    public static void interestCalc() {
+        System.out.println("Sounds111");
+     }
+
+
 
 }
 
